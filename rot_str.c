a2970682e68daf81aa13b41f534eea933ba1e499
YoussefKhaled03 <ykte03@gmail.com>
2023-09-19 20:13:26 +0200

    el nekaba

---
+++ rot_str.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rot_str - rot13
+ * @s: the char 
+ * return: size of strig
+ */
+
+int rot_str(char *s)
+{
+	int i;
+
+	if (!s)
+		return (_putstring("(null)"));
+	for (i = 0; s[i]; i++)
+		{
+			if (s[i] >= 'A' && s[i] <= 'Z')
+			{
+				if (s[i] - 'A' < 13)
+					_putchar(s[i] + 13);
+				else
+					_putchar(s[i] - 13);
+			}
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				
+                                if (s[i] - 'a' < 13)
+                                        _putchar(s[i] + 13);
+                                else
+                                        _putchar(s[i] - 13);
+			}
+}
+
